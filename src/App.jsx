8fb2e82cc8f8400af2e688bbb1f767898ec19126
eliearja <elie.arja8@gmail.com>
2023-08-05 19:44:05 -0400

    Fixing path in App.jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
-import Education from "./pages/Courses/Education";
+import Education from "./pages/Education/Education";
 import Experience from "./pages/Experience/Experience";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
