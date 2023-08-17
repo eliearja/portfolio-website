2a8d056be36b24a4583cc5f0d3a3956381d125c5
eliearja <elie.arja8@gmail.com>
2023-08-17 19:55:33 -0400

    Preparing for changes to GradePanel component

--- src/pages/Education/components/GradePanel/GradePanel.jsx
+++ src/pages/Education/components/GradePanel/GradePanel.jsx
@@ -5,16 +5,19 @@ function GradePanel({ closePanel }) {
   return (
     <div className="GradePanelWrapper">
       <div className="GradePanel">
-        <h2>Grade Panel</h2>
-        <p>To be filled soon...</p>
-        <button
-          className="CloseButton"
-          onClick={() => {
-            closePanel(false);
-          }}
-        >
-          Close
-        </button>
+        <div className="GradePanelBody">
+          <p>To be filled soon...</p>
+        </div>
+        <div className="GradePanelFooter">
+          <button
+            className="CloseButton"
+            onClick={() => {
+              closePanel(false);
+            }}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
